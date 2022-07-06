47f251fc522ee55ed9f24fe4ecacde04c95bf808
chaukle <50463823+chaukle@users.noreply.github.com>
2022-07-07 00:46:33 +0200

    Setting the domain model

---
+++ Passenger.Core/Passenger.Core/Domain/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Passenger.Core.Domain
+{
+    public class Node
+    {
+        public string Adress { get; protected set; }
+        public double Longidute { get; protected set; }
+        public double Latitude { get; protected set; }
+        
+    }
+}
---
+++ Passenger.Core/Passenger.Core/Domain/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace Passenger.Core.Domain
+{
+    public class Vehicle
+    {
+        public string Brands  { get; set; }
+        public string Name { get; set; }
+        public int Seats { get; set; }
+    }
+}
